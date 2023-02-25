fa29b7e8d03d487042685b877352cd242504eb20
AlxV05 <alexander.kai.chen@gmail.com>
2023-02-25 14:19:31 -0600

    pascal formula exercise

---
+++ chapter_one/one_two/one_two_pascal_formula.py
@@ -0,0 +1,18 @@
+def pascal_iter(current_level, max_level, data):
+    if len(data) == 0:
+        x = [1]
+    else:
+        x = [0 for i in range(len(data)+1)]
+        for i in range(len(data)):
+            x[i] += data[i]
+            x[i+1] += data[i]
+    print(x)
+    if current_level < max_level:
+        pascal_iter(current_level+1, max_level, x)
+
+
+def pascal_formula(level):
+    pascal_iter(1, level, [])
+
+
+pascal_formula(7)
