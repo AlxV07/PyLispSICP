d021c306cb5b2c1586b7627bf7cbed571246b68f
alxv05 <alexander.kai.chen@gmail.com>
2023-03-01 15:39:20 -0600

    fermat's theorem

---
+++ chapter_one/one_two/one_two_fermat_theorem.py
@@ -0,0 +1,14 @@
+def exp_mod(base, exp, m):
+    if exp == 0:
+        return 1
+    elif exp % 2 == 0:
+        x = exp_mod(base, (exp / 2), m)
+        return (x * x) % m
+    else:
+        return (base * exp_mod(base, exp - 1, m)) % m
+
+
+def fermat_theorem(n):
+    def run(a):
+        return exp_mod(a, n, n) == a
+    return run(n)
