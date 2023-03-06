490d1c7dd3532681c6452e6d46406dabf459bdb6
alxv05 <alexander.kai.chen@gmail.com>
2023-03-06 13:46:51 -0600

    fast prime timed test

--- chapter_one/one_two/one_two_is_prime_formula.py
+++ chapter_one/one_two/one_two_is_prime_formula.py
@@ -1,6 +1,6 @@
 def smallest_divisor(n):
     if n <= 1:
-        return n-1
+        return n - 1
     else:
         return find_divisor(n, 2)
 
@@ -16,3 +16,24 @@ def find_divisor(n, divisor):
 
 def is_prime(n):
     return n == smallest_divisor(n)
+
+
+# Optimized smallest_divisor
+def better_smallest_divisor(n):
+    if n <= 1:
+        return n - 1
+    else:
+        return better_find_divisor(n, 2)
+
+
+def better_find_divisor(n, divisor):
+    if divisor * divisor > n:
+        return n
+    elif n % divisor == 0:
+        return divisor
+    else:
+        return find_divisor(n, next_test_int(divisor))
+
+
+def next_test_int(current_int):
+    return 3 if current_int == 2 else current_int + 2
--- chapter_one/one_two/one_two_timed_prime_test.py
+++ chapter_one/one_two/one_two_timed_prime_test.py
@@ -1,4 +1,6 @@
 import time
+
+from chapter_one.one_two.one_two_fast_prime import fast_prime
 from one_two_is_prime_formula import is_prime
 
 
@@ -15,3 +17,14 @@ def start_prime_test(n, start_time):
 def report_prime(elapsed_time):
     print(elapsed_time)
     print("***")
+
+
+# Timed prime test using fast_prime
+def timed_fast_prime_test(n):
+    start_fast_prime_test(n, time.time())
+
+
+def start_fast_prime_test(n, start_time):
+    if fast_prime(n, int(n * 0.7)):
+        print(n)
+        report_prime(time.time() - start_time)
