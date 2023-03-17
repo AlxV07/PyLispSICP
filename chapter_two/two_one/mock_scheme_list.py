24c34e13554c486cd38a78e1046a6f345249c01f
AlxV05 <alexander.kai.chen@gmail.com>
2023-03-16 22:02:23 -0500

    mock list ref, length

--- chapter_two/two_one/mock_scheme_list.py
+++ chapter_two/two_one/mock_scheme_list.py
@@ -1,4 +1,4 @@
-from chapter_two.one_one.mock_scheme_lisp_pair import cons, car, cdr
+from chapter_two.one_one.mock_scheme_lisp_pair import cons, car, cdr, Pair
 
 def mock_lisp_list(*args):
     return mock_lisp_list_backend(tuple(args))
@@ -8,3 +8,20 @@ def mock_lisp_list_backend(args):
         return args[0]
     else:
         return cons(args[0], mock_lisp_list_backend(args[1:]))
+
+def mock_lisp_list_ref(i, n):
+    if type(i) != Pair:
+        return i
+    elif n == 0:
+        return car(i)
+    else:
+        return mock_lisp_list_ref(cdr(i), n - 1)
+
+def mock_lisp_list_length(i):
+    pass
+    if i is None:
+        return 0
+    elif type(i) != Pair:
+        return 1
+    else:
+        return 1 + mock_lisp_list_length(cdr(i))
