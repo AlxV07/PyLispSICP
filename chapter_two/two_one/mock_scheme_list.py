632f71ad9689b843972658507c1f2c2cad58265e
AlxV05 <alexander.kai.chen@gmail.com>
2023-03-16 19:26:02 -0500

    scheme-lisp mock list

---
+++ chapter_two/two_one/mock_scheme_list.py
@@ -0,0 +1,14 @@
+from chapter_two.one_one.mock_scheme_lisp_pair import cons, car, cdr
+def mock_lisp_list(*args):
+    return mock_lisp_list_backend(tuple(args))
+
+def mock_lisp_list_backend(args):
+    if len(args) == 1:
+        return args[0]
+    else:
+        return cons(args[0], mock_lisp_list_backend(args[1:]))
+
+x = mock_lisp_list(1, 2, 3)
+print(car(x))
+print(car(cdr(x)))
+print(cdr(cdr(x)))
